3bf0e4485ed3241328690f3c0d4f763a7a3c1e35
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-09-16 16:22:48 +0600

    state-variable-fn

--- hello.sol
+++ hello.sol
@@ -6,4 +6,10 @@ contract SolidityTest {
    constructor() public {
       storedData = 10; 
    }
+   function getResult() public view returns(uint){
+    uint a = 1;
+    uint b = 2;
+    uint result = a+b;
+    return result
+   }
 }
