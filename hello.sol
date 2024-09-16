47c2245d3de73132dc80ab8725d38cf7cb8befdf
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-09-16 16:12:00 +0600

    address

--- hello.sol
+++ hello.sol
@@ -2,5 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract HelloWorld {
-    string public greet = "Hello, World!";
+
+address x = 0x212;
+address myAddress = this;
+if (x.balance < 10 && myAddress.balance >= 10) x.transfer(10);
+
 }
