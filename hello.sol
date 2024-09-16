1ef0c302baff7b3961c5f71956e1c3d2a42c4782
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-09-16 16:18:42 +0600

    state-variable

--- hello.sol
+++ hello.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract HelloWorld {
-
-address x = 0x212;
-address myAddress = this;
-if (x.balance < 10 && myAddress.balance >= 10) x.transfer(10);
-
+contract SolidityTest {
+   uint storedData;     
+   constructor() public {
+      storedData = 10; 
+   }
 }
