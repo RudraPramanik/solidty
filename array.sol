4451acddfc122d694d3848186e51a05ad2a91ca7
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-09-17 23:48:05 +0600

    array

---
+++ array.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+
+// type[] arrayName;
+
+contract solidityArray{
+    uint balance[3] = [1, 2, 3];
+    // to declare it without amount
+    //accessing array elements
+    uint salary = balance[2]
+}
---
+++ string.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+contract solidityString{
+    constructor() public{}
+    function getResult() public view returns(string memory) {
+
+    }
+
+}
