fa44a6bc9f6ebf4cf073fd188a0f95a21f5e8b5d
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-09-18 00:02:25 +0600

    accessing array elements

--- array.sol
+++ array.sol
@@ -8,4 +8,25 @@ contract solidityArray{
     // to declare it without amount
     //accessing array elements
     uint salary = balance[2]
+    function testArray() public pure{
+        uint len = 7;
+
+        //dynamic array
+        dynamic[] memory a = new uint[](7);
+
+        bytes memory b = new bytes(len)
+
+        assert(a.length == 7);
+        assert(b.length==len);
+
+        //access array variable
+        a[6] = 8;
+
+        //test array vaiable
+        assert(a[6] == 8 );
+
+        //static array
+        uint[3] memory c = [uint(1), 2, 3];
+        assert(c.length == 3);
+    }
 }
