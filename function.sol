58e2ca7f127c3f4d1e92f5f0758e5a187009bced
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-09-21 13:18:23 +0600

    function

---
+++ function.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract firsFunction{
+    constructor() public{}
+
+    function getResult() public view returns(string memory){
+        uint a = 1;
+        uint b = 2;
+        uint result =  a+b
+        return integerToString = result
+    }
+}
