d67c6311117a7a5d16486ba33fb934d25e3843a1
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-09-26 19:10:10 +0600

    modefier

--- fn_modifier.sol
+++ fn_modifier.sol
@@ -1,27 +1,36 @@
 pragma solidity ^0.8.0;
 
 contract Owner {
-    address owner;
-   modifier onlyOwner {
-      require(msg.sender == owner);
-      _;
-   }
-   modifier costs(uint price) {
-      if (msg.value >= price) {
-         _;
-      }
-   }
+    address public owner;
+
+    constructor() {
+        owner = msg.sender; // Initialize the owner
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Not the contract owner");
+        _;
+    }
+
+    modifier costs(uint price) {
+        require(msg.value >= price, "Insufficient funds sent");
+        _;
+    }
 }
 
-contract Register is Owner{
-    mapping (address => bool) registeredAddressses;
-    uint price;
-    constructor( uint initialPrice ) public {price = initialPrice}
+contract Register is Owner {
+    mapping(address => bool) public registeredAddresses;
+    uint public price;
 
-    function Register() public payable costs(price){
-        registeredAddressses[msg.sender] = true;
+    constructor(uint initialPrice) {
+        price = initialPrice;
     }
-    function changePrice(uint _price) public onlyOwner{
-        price = _price
+
+    function register() public payable costs(price) {
+        registeredAddresses[msg.sender] = true;
+    }
+
+    function changePrice(uint _price) public onlyOwner {
+        price = _price;
     }
 }
