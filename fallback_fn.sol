82910174466c61bd3e5025cdc8c8ec2250898a16
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-10-01 14:38:25 +0600

    fallback fn

--- fallback_fn.sol
+++ fallback_fn.sol
@@ -1,35 +1,34 @@
-//  special function available to a contract
-//  It is called when a non-existent function is called on the contract.
-//  It is required to be marked external.
-//  It has no name.
-//  It has no arguments
-//  It can not return any thing.
-//  It can be defined one per contract.
-
 pragma solidity ^0.8.0;
 
 contract Test {
-   uint public x ;
-   function() external { x = 1; }    
+    uint public x;
+
+    fallback() external {
+        x = 1;
+    }
 }
 
 contract Sink {
-   function() external payable { }
+    fallback() external payable {
+        // Can receive Ether
+    }
 }
+
 contract Caller {
-   function callTest(Test test) public returns (bool) {
-      (bool success,) = address(test).call(abi.encodeWithSignature("nonExistingFunction()"));
-      require(success);
-      // test.x is now 1
+    function callTest(Test test) public returns (bool) {
+        (bool success, ) = address(test).call(abi.encodeWithSignature("nonExistingFunction()"));
+        require(success, "Call to Test contract failed");
+        // test.x is now 1
 
-      address payable testPayable = address(uint160(address(test)));
+        address payable testPayable = payable(address(test));
 
-      // Sending ether to Test contract,
-      // the transfer will fail, i.e. this returns false here.
-      return (testPayable.send(2 ether));
-   }
-   function callSink(Sink sink) public returns (bool) {
-      address payable sinkPayable = address(sink);
-      return (sinkPayable.send(2 ether));
-   }
+        // Attempt to send Ether to Test contract (will fail)
+        return testPayable.send(2 ether);
+    }
+
+    function callSink(Sink sink) public returns (bool) {
+        address payable sinkPayable = payable(address(sink));
+        // Send Ether to Sink contract (will succeed)
+        return sinkPayable.send(2 ether);
+    }
 }
