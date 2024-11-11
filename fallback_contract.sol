3a4fa64a2e07e7ad95b96ac494da2d61dbfd3d2c
Rudra <rudrahimangshoo2020@gmail.com>
2024-11-11 10:13:40 +0600

    Update fallback_contract.sol

--- fallback_contract.sol
+++ fallback_contract.sol
@@ -1,1 +1,24 @@
 
+pragma solidity ^0.8.0;
+
+
+contract fallback{
+  event log(string func, uint gas)
+
+//fallback fn must be  declared as external
+  fallback() external payable{
+//send/transfer 2300 ether atleast
+   emit Log("recieve", gasleft())
+  }
+
+// recieve a varient of fall back wich trigger msg.call
+recieve() external payable{
+emit log("recieve", gasleft())
+}
+
+//helper fun to check the balance of the contract
+function getBalance() public view returns(uint){
+ return address(this).balance;
+}
+
+}
